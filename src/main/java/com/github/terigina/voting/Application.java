16b6b24163adee3e9d210f4da21e1bd6ba4092c0
Taisia Yerygina <t.yerygina@gmail.com>
2021-02-20 14:05:15 +0100

    configurable port

--- src/main/java/com/github/terigina/voting/Application.java
+++ src/main/java/com/github/terigina/voting/Application.java
@@ -1,10 +1,16 @@
 package com.github.terigina.voting;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+import static spark.Spark.staticFiles;
 
 public class Application {
 
+    private static final int PORT = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+
     public static void main(String[] args) {
+        port(PORT);
         staticFiles.location("/public");
         get("/vote/options", (req, res) -> {
             res.type("application/json");
